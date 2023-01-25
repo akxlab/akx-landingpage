df5096fa0a4477a134978dd8c024a3be0483d809
AKX Labz <97577935+quantosnetwork@users.noreply.github.com>
2023-01-25 11:33:24 -0500

    currency switcher

--- landing/components/home/Presale.vue
+++ landing/components/home/Presale.vue
@@ -19,6 +19,8 @@ const { t } = useI18n()
 
       </v-col>
       <v-col cols="12">
+
+
         <PaymentSimple />
       </v-col>
     </v-row>
--- landing/components/payment/Simple.vue
+++ landing/components/payment/Simple.vue
@@ -31,6 +31,25 @@ onMounted(async () => {
 
       <v-form>
         <v-row>
+          <v-col cols="12" lg="10">
+            <v-toolbar color="transparent" density="compact" style="width:50%">
+              <v-toolbar-title>
+                Switch currency
+              </v-toolbar-title>
+              <v-toolbar-items>
+                <v-chip class="mr-3">USD</v-chip>
+                <v-chip class="mr-3">CAD</v-chip>
+                <v-chip class="mr-3">EURO</v-chip>
+                <v-chip class="mr-3">ETH</v-chip>
+                <v-chip class="mr-3">MATICS</v-chip>
+              </v-toolbar-items>
+            </v-toolbar>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" >
+
+          </v-col>
           <v-col cols="12" lg="3"><v-text-field type="number"  autofocus single-line v-model="amountIn" prefix="you send" :suffix="currIn" append-icon="fa-light fa-arrow-right"></v-text-field></v-col>
           <v-col cols="12" lg="3"><v-text-field type="number" autofocus single-line v-model="amountOut" prefix="you get" suffix="AKX"></v-text-field></v-col>
           <v-col cols="12" lg="3"><v-btn variant="elevated" size="x-large" color="#00DC82FF" style="color:black">CONFIRM YOUR ORDER</v-btn></v-col>
